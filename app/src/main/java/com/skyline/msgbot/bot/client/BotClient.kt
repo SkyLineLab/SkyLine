ab742db7025052f798f95eed1133682958e86071
naijun0403 <65700898+archethic@users.noreply.github.com>
2022-01-25 23:47:46 +0900

    update readme

--- app/src/main/java/com/skyline/msgbot/bot/client/BotClient.kt
+++ app/src/main/java/com/skyline/msgbot/bot/client/BotClient.kt
@@ -10,7 +10,7 @@ import com.skyline.msgbot.bot.event.MessageEvent
 import org.graalvm.polyglot.Value
 
 class BotClient() {
-    private val eventMap: HashMap<String, Value> = hashMapOf();
+    private val eventMap: HashMap<String, Value> = hashMapOf()
 
     fun on(event: String, function: Value): BotClient {
         eventMap[event] = function
@@ -25,10 +25,10 @@ class BotClient() {
     fun emit(event: String, arguments: MessageEvent): Boolean { //vararg arguments: Any
         println("emit = $event")
         if (!eventMap.containsKey(event)) return false
-        val func: Value = eventMap[event] ?: return false
         object : Thread() {
             override fun run() {
-                func.executeVoid(arguments)
+                val func: Value? = eventMap[event]
+                func?.executeVoid(arguments)
             }
         }.start()
         println("call!")
--- readme.md
+++ readme.md
@@ -8,3 +8,13 @@ The Rhino engine also supports ES2015 poorly, while the GraalJS supports the maj
 ### Warning
 `However, GraalJS does not yet support Android.
 We modified the part that doesn't work on Android and changed it to work on Android.`
+## example
+```javascript
+const client = BotProject.getClient();
+
+client.on('message', (data) => {
+    if (data.message === '!ping') {
+        data.room.send('pong!');
+    }
+});
+```
