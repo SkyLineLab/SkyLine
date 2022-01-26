1b8d6c2227bb2d842b617860e1748caf452d3d72
EunDaeng <verp@daum.net>
2022-01-26 15:07:05 +0900

    Requesting Permission Easier

--- app/src/main/java/com/skyline/msgbot/MainActivity.kt
+++ app/src/main/java/com/skyline/msgbot/MainActivity.kt
@@ -17,6 +17,10 @@ import com.skyline.msgbot.ui.theme.SkyLineTheme
 import com.skyline.msgbot.utils.ForegroundTask
 
 class MainActivity : ComponentActivity() {
+    override fun onStart() {
+        super.onStart()
+        PermissionUtil.isAllowPermision(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,17 +34,12 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        if (!PermissionUtil.requestStoragePermissions(this)) {
-            Toast.makeText(this, "파일 권한이 없습니다 :( 권한을 허용해주세요", Toast.LENGTH_LONG).show()
+        if(PermissionUtil.isAllowPermision(this)) {
+            if (!RuntimeManager.hasRuntime("test")) {
+                RuntimeManager.addRuntime("test")
+            }
+            ForegroundTask.startForeground(this)
         }
-        if (!PermissionUtil.getAccessNotification(this)) { //알림 권한 체크
-            Toast.makeText(this, "알림 읽기 권한이 없습니다 :( 권한을 허용해주세요", Toast.LENGTH_LONG).show()
-            PermissionUtil.requestAccessNotification(this)
-        }
-        if (!RuntimeManager.hasRuntime("test")) {
-            RuntimeManager.addRuntime("test")
-        }
-        ForegroundTask.startForeground(this)
     }
 }
 
--- app/src/main/java/com/skyline/msgbot/utils/PermissionUtil.kt
+++ app/src/main/java/com/skyline/msgbot/utils/PermissionUtil.kt
@@ -10,22 +10,25 @@ import android.os.Build
 import android.os.Environment
 import android.os.PowerManager
 import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.core.app.ActivityCompat.*
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.PermissionChecker
-import androidx.core.content.PermissionChecker.checkSelfPermission
 
 object PermissionUtil {
     fun getAccessNotification(context: Context): Boolean {
         return NotificationManagerCompat.getEnabledListenerPackages(context).contains(context.packageName)
     }
 
-    fun requestAccessNotification(context: Context) {
-        val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        context.startActivity(intent)
+    fun requestAccessNotification(context: Context): Boolean {
+        return if(!getAccessNotification(context)) {
+            val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            context.startActivity(intent)
+            Toast.makeText(context, "알림 읽기 권한이 없습니다 :( 권한을 허용해주세요", Toast.LENGTH_LONG).show()
+            false
+        } else true
     }
 
     fun getIgnoreBatteryOptimizations(context: Context): Boolean {
@@ -33,11 +36,15 @@ object PermissionUtil {
         return powerCheck.isIgnoringBatteryOptimizations(context.packageName)
     }
 
-    fun requestIgnoreBatteryOptimizations(context: Context) {
-        val intent = Intent()
-        intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
-        intent.data = Uri.parse("package:${context.packageName}")
-        context.startActivity(intent)
+    fun requestIgnoreBatteryOptimizations(context: Context): Boolean {
+        return if(getIgnoreBatteryOptimizations(context)) true
+        else {
+            val intent = Intent()
+            intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+            intent.data = Uri.parse("package:${context.packageName}")
+            context.startActivity(intent)
+            false
+        }
     }
 
     fun requestStoragePermissions(context: Context): Boolean {
@@ -52,6 +59,7 @@ object PermissionUtil {
             }
             else {
                 context.startActivity(Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION))
+                Toast.makeText(context, "파일 권한이 없습니다 :( 권한을 허용해주세요", Toast.LENGTH_LONG).show()
             }
         } else {
             return if(ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
@@ -62,6 +70,7 @@ object PermissionUtil {
                     context as Activity,
                     permissions, 0
                 )
+                Toast.makeText(context, "파일 권한이 없습니다 :( 권한을 허용해주세요", Toast.LENGTH_LONG).show()
                 false
             }
         }
@@ -69,10 +78,6 @@ object PermissionUtil {
     }
 
     fun isAllowPermision(context: Context): Boolean {
-        if(!getAccessNotification(context) || !getIgnoreBatteryOptimizations(context) ||
-            (checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_DENIED || checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_DENIED)) {
-            return false;
-        }
-        return true;
+        return requestStoragePermissions(context) && requestAccessNotification(context) && requestIgnoreBatteryOptimizations(context)
     }
 }
