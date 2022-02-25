cc3468903879189a51250648a051d29d1a8b51a9
naijun0403 <65700898+archethic@users.noreply.github.com>
2022-02-25 21:07:34 +0900

    add javax.annotation

--- app/src/main/java/com/skyline/msgbot/bot/project/BotProject.kt
+++ app/src/main/java/com/skyline/msgbot/bot/project/BotProject.kt
@@ -6,16 +6,54 @@
 
 package com.skyline.msgbot.bot.project
 
+import com.skyline.msgbot.bot.api.FileStream
 import com.skyline.msgbot.bot.client.BotClient
 import com.skyline.msgbot.bot.runtime.RuntimeManager
+import com.skyline.msgbot.bot.util.ApiApplyUtil
+import com.skyline.msgbot.bot.util.ContextUtils
+import com.skyline.msgbot.setting.Constants
+import com.skyline.msgbot.utils.SDCardUtils
+import org.graalvm.polyglot.Context
+import org.graalvm.polyglot.Source
 
 class BotProject(val runtimeID: Number) {
     fun getClient(): BotClient? {
         return RuntimeManager.clients[runtimeID];
     }
 
-    fun on(): Boolean {
-        RuntimeManager.runtimes.remove(runtimeID)
-        return true
+    fun getAllProjectNames(): List<String> {
+        return RuntimeManager.projectNames.keys.toList()
+    }
+
+    fun getCurrentProjectId(): Number {
+        return runtimeID
+    }
+
+    fun getCurrentProjectContext(): Context? {
+        return RuntimeManager.runtimes[runtimeID]
+    }
+
+    fun compile(): Boolean {
+        try {
+            Thread {
+                for (runtime in RuntimeManager.runtimes) {
+                    println(runtime)
+                    val cx = ContextUtils.getJSContext()
+                    ApiApplyUtil.applyBotApi(cx.getBindings("js"), true, runtime.key)
+                    RuntimeManager.runtimes[runtime.key] = cx
+                    RuntimeManager.runtimes[runtime.key]?.eval(
+                        Source.create(
+                            "js",
+                            FileStream.read("${SDCardUtils.sdcardPath}/${Constants.directoryName}/${RuntimeManager.projectIds[runtime.key]}/script.js")
+                        )
+                    )
+                }
+            }.start()
+
+            return true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
     }
 }
--- app/src/main/java/com/skyline/msgbot/bot/util/ContextUtils.kt
+++ app/src/main/java/com/skyline/msgbot/bot/util/ContextUtils.kt
@@ -10,19 +10,23 @@ import com.skyline.msgbot.setting.Constants
 import com.skyline.msgbot.utils.SDCardUtils
 import org.graalvm.polyglot.Context
 import org.graalvm.polyglot.HostAccess
+import org.graalvm.polyglot.PolyglotAccess
 
 object ContextUtils {
     fun getJSContext(): Context {
         return Context.newBuilder("js")
             .allowHostAccess(HostAccess.ALL)
+            .allowPolyglotAccess(PolyglotAccess.ALL)
             .allowExperimentalOptions(true)
             .allowIO(true)
             .option("js.commonjs-require", "true")
             .option("js.commonjs-require-cwd", "${SDCardUtils.sdcardPath}/${Constants.directoryName}/")
             .option("js.commonjs-global-properties", "/data/user/0/com.skyline.msgbot/files/lib/global.js")
+            .option("js.foreign-object-prototype", "true")
             .option("js.syntax-extensions", "true")
             .option("js.nashorn-compat", "true")
             .option("js.ecmascript-version", "2022")
+//            .option("js.webassembly", "true")
             .allowHostClassLookup { true }.build()
     }
 }
