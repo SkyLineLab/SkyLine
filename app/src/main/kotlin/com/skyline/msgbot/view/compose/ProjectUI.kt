61757f96c2855695e829ca5895b67e12b71f02fa
EunDaeng <verp@daum.net>
2022-08-11 18:58:01 +0900

    Update ProjectUI.kt
    text "+" fontsize

--- app/src/main/kotlin/com/skyline/msgbot/view/compose/ProjectUI.kt
+++ app/src/main/kotlin/com/skyline/msgbot/view/compose/ProjectUI.kt
@@ -6,11 +6,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,7 +37,7 @@ fun ProjectUI() {
             onClick = {
                 //TODO
             }) {
-            Text("+", fontSize = 50.sp, modifier = Modifier.align(Alignment.Top))
+            Text("+", fontSize = 35.sp, modifier = Modifier.align(Alignment.Top))
         }
     }
 }
